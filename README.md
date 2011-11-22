6c09bdaade8e5a88e5867dd8c836f1b5c9296804
PokerChang <chang@pokerchang.com>
2011-11-22 10:50:33 -0800

    in progress

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-<h1>Merge an Android strings.xml into Ruby on Rails i18n yml file</h1>
+<h1>Convert iOS localization to Android</h1>
 <h1>Usage</h1>
-ruby convert.rb android_strings_xml_path rails_yml_path
+ruby convert.rb ios_project_path android_project_path
 <h1>Description</h1>
-This simple ruby script will merge Android strings file into Ruby on Rails i18n yml file.  The result is sorted alphabetically.  It's safe to rerun.  Current only English is tested but it can be easily convert to other languages.
+This simple ruby script will convert iOS localizations to Android.  The result is sorted alphabetically.  It's safe to rerun.  Currently only English and Chinese are tested but it can be easily convert to other languages.
 
 <h1>TODO</h1>
 <ol>
 <li>Merge both ways</li>
-<li>Automatically enumerate all possible android locales and create according to locales for Rails</li>
+<li>Add Support for Rails</li>
 </ol>
--- convert.rb
+++ convert.rb
@@ -1,43 +1,99 @@
 require 'rexml/document'
-banner = 'Usage: ruby convert.rb android_strings_xml_path rails_yml_path'
+require 'builder'
+
+def product_xml
+  xml = Builder::XmlMarkup.new( :indent => 2 )
+  xml.instruct! :xml, :encoding => "UTF-8"
+  xml.product do |p|
+    p.name "Test"
+  end
+end
+
+
+
+banner = 'Usage: ruby convert.rb iOS_project_path android_project_path_with_res.'
 
 if ARGV.count < 2
   puts banner
   exit
 end
-file   = File.open(ARGV[1], "r")
+mapping = Hash.new
+mapping["zh_Hans.lproj"] = 'values-zh'
+mapping["zh_Hant.lproj"] = 'values-zh-rTW'
+mapping["ru.lproj"] = 'values-ru'
 
-xml = File.read(ARGV[0])
-doc= REXML::Document.new(xml)
-
-# order alphabetically
-result = []
-doc.elements.each('/resources/string') do |p|
-  result << ('  ' + p.attributes['name']+': "'+ p.text.gsub(/\n/,' ').squeeze(' ') + '"')
-end
-result = result
-
-
-
-output = []
-header = []
-while (line = file.gets)
-  if line.start_with? ' '
-    output << line.chop
-  else
-    header << line.chop
+def read_android(path)
+  result = Hash.new  
+  if File.exists?(path)
+    xml = File.read(path)
+    doc= REXML::Document.new(xml)
+    # order alphabetically
+    doc.elements.each('/resources/string') do |p|
+      result[p.attributes['name']] = p.text.gsub(/\n/,' ').squeeze(' ')
+    end
   end
+  return result
 end
 
-result = (output | result).sort{|a, b| a.downcase <=> b.downcase}
+i = 0
+Dir["#{ARGV[0]}/**.lproj"].each do |language|
+  puts "Processing #{language}..."
+  short_name = language[language.rindex('/')+1..-1]
+  #output = thisfile.gsub('xib', 'strings')
+  filenum = 0;
+  # 1. Read the iOS localization  
+  result=Hash.new
+  Dir["#{ARGV[0]}/#{language}/**.strings"].each do |filename|
+    puts "Processing #{filename}..."    
+    file = File.new(filename, "r")
+    counter = 1
+    while (line = file.gets)
+      separator = '" = "'
+      parts = line.partition separator
+      if parts[1] == separator
+        result[parts[0][1..-1]] = parts[2].chomp '"'
+      end
+      counter = counter + 1
+    end
+    file.close
+    file = File.read(filename)
+    filenum += 1
+  end
+  puts "String files:#{filenum}"  
+  output = mapping[short_name]
+  if output.nil?
+    # use default
+    code = short_name[0..short_name.index('.')-1]    
+    output = "values-#{code}"
+    puts "Unknown mapping for #{short_name}.  Using default #{output}"
+  end
 
-routput = File.open(ARGV[1], "w")
-header.each do |r|
-  routput.puts r
-  #puts r
+  full_output = "#{ARGV[1]}/#{output}/strings.xml"
+  puts "Writing to #{full_output}"
+  # 2. read the android original
+  original = read_android("#{ARGV[1]}/values/strings.xml")
+  original.each do |key, value|
+    if result[key].nil? 
+      result[key] = value
+    end
+  end
+  
+  # 3. read the android localization
+  old = read_android(full_output)
+  old.each do |key, value|
+    result[key] = value
+  end
+  
+  #write to android  
+  puts result
+  xml = Builder::XmlMarkup.new( :indent => 2 )
+  xml.instruct! :xml, :encoding => "UTF-8"
+  xml.resources do |r|
+    result.each do |key, value|
+      r.string value
+    end
+  end
+  i = i + 1  
 end
 
-result.each do |r|
-  routput.puts r
-  #puts r
-end
+puts "Languages:#{i}"
